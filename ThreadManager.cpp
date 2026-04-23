fe8fcab47e0f3a5909d838f3108c3430e9a2c230
Dor Shneider <dor.shneider@example.com>
2026-04-23 17:24:57 +0300

    finished 2

--- ThreadManager.cpp
+++ ThreadManager.cpp
@@ -6,10 +6,10 @@ typedef unsigned long address_t;
 address_t translate_address(address_t addr)
 {
     address_t ret;
-    asm volatile("xor    %%gs:0x18,%0\n"
-                 "rol    $0x9,%0\n"
-    : "=g" (ret)
-    : "0" (addr));
+    asm volatile("xor    %%fs:0x30,%0\n"
+                 "rol    $0x11,%0\n"
+                 : "=g" (ret)
+                 : "0" (addr));
     return ret;
 }
 
